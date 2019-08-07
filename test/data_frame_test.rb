6659d4ff73c71722d9c34b7b2759034a620dfff6
Andrew Kane <andrew@chartkick.com>
2019-08-07 05:44:48 -0700

    Test row indices [skip ci]

--- test/data_frame_test.rb
+++ test/data_frame_test.rb
@@ -15,6 +15,7 @@ class DataFrameTest < Minitest::Test
     assert_equal ["a", "b", "c"], df["c1"]
     assert_equal Eps::DataFrame.new(c1: ["a", "b", "c"]), df[["c1"]]
     assert_equal df[0..1], df[0...-1]
+    assert_equal Eps::DataFrame.new(c1: ["a", "c"], c2: [1, 3]), df[[0, 2]]
 
     error = assert_raises do
       df[0.., "c3"]
