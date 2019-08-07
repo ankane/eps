d3473e37449f4266caf18287fbb0ef84e08e0d32
Andrew Kane <andrew@chartkick.com>
2019-08-07 11:38:45 -0700

    Move to xenial on Travis

--- test/data_frame_test.rb
+++ test/data_frame_test.rb
@@ -3,13 +3,13 @@ require_relative "test_helper"
 class DataFrameTest < Minitest::Test
   def test_subsetting
     df = Eps::DataFrame.new(c1: ["a", "b", "c"], c2: [1, 2, 3])
-    assert_equal df, df[0..]
+    assert_equal df, df[0..nil]
     assert_equal Eps::DataFrame.new(c1: ["b"], c2: [2]), df[1]
-    assert_equal Eps::DataFrame.new(c2: [2, 3]), df[1.., ["c2"]]
+    assert_equal Eps::DataFrame.new(c2: [2, 3]), df[1..nil, ["c2"]]
     assert_equal Eps::DataFrame.new(c1: ["c"], c2: [3]), df[-1]
-    assert_equal [2, 3], df[1.., "c2"]
-    assert_equal df, df[0.., "c1".."c2"]
-    refute_equal df, df[0.., "c2".."c1"]
+    assert_equal [2, 3], df[1..nil, "c2"]
+    assert_equal df, df[0..nil, "c1".."c2"]
+    refute_equal df, df[0..nil, "c2".."c1"]
     assert_equal df[["c1"]], df[0.., "c1"..."c2"]
     assert_equal Eps::DataFrame.new(c2: [3], c1: ["c"]), df[2.., "c2".."c1"]
     assert_equal ["a", "b", "c"], df["c1"]
