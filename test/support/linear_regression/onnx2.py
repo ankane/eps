62add90ab0e88d72a27884f79307ff89923cca8d
Andrew Kane <andrew@chartkick.com>
2019-08-29 18:22:59 -0700

    Updated onnx2 [skip ci]

--- test/support/linear_regression/onnx2.py
+++ test/support/linear_regression/onnx2.py
@@ -5,15 +5,18 @@ from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.preprocessing import FunctionTransformer
 
-df = pd.read_csv("test/support/data/houses.csv")
+df = pd.read_csv("test/support/data/mpg.csv")
 # print(df)
 
-X = df[['bedrooms', 'bathrooms', 'state', 'color']]
-y = df['price']
+X = df[['displ', 'year', 'cyl', 'trans', 'drv', 'class', 'model']]
+y = df['hwy']
 
-numeric_features = [0, 1]
-categorical_features = [2, 3]
+numeric_features = [0, 1, 2]
+categorical_features = [3, 4, 5]
+text_features = [6]
 
 # numeric_transformer = Pipeline(steps=[
 #     ('imputer', SimpleImputer(strategy='median'))
@@ -25,11 +28,19 @@ categorical_transformer = Pipeline(steps=[
     # ('ordinal', OrdinalEncoder())
 ])
 
+text_transformer = Pipeline(steps=[
+    ('vect', CountVectorizer(max_features=10)),
+    ('tfidf', TfidfTransformer()),
+    # ('dense', FunctionTransformer(lambda x: print(x.todense()), accept_sparse=True))
+])
+
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', 'passthrough', numeric_features),
-        ('cat', categorical_transformer, categorical_features)
+        ('cat', categorical_transformer, categorical_features),
+        ('txt', text_transformer, text_features)
     ]
+    # sparse_threshold=0
 )
 
 estimators = [
@@ -41,31 +52,34 @@ pipe.fit(X, y)
 print(X[:1])
 print(pipe.predict(X[:1]))
 
+import sys
+sys.exit(1)
+
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType, StringTensorType
-initial_type = [('numfeat', FloatTensorType([1, 2])),
-                ('strfeat', StringTensorType([1, 2]))]
+initial_type = [('numfeat', FloatTensorType([-1, 3])),
+                ('catfeat', StringTensorType([-1, 3])),
+                ('txtfeat', StringTensorType([-1, 1]))]
 onnx = convert_sklearn(pipe, initial_types=initial_type)
 with open("test/support/linear_regression/houses.onnx", "wb") as f:
   f.write(onnx.SerializeToString())
 
-
 # from onnx.tools.net_drawer import GetPydotGraph, GetOpNodeProducer
-# pydot_graph = GetPydotGraph(onnx.graph, name=onnx.graph.name, rankdir="TP",
-#                             node_producer=GetOpNodeProducer("docstring"))
-# pydot_graph.write_dot("graph.dot")
+pydot_graph = GetPydotGraph(onnx.graph, name=onnx.graph.name, rankdir="TP",
+                            node_producer=GetOpNodeProducer("docstring"))
+pydot_graph.write_dot("graph.dot")
 
-# import os
-# os.system('dot -O -Tpng graph.dot')
+import os
+os.system('dot -O -Tpng graph.dot')
 
 # import numpy
-import onnxruntime as rt
-sess = rt.InferenceSession("test/support/linear_regression/houses.onnx")
-print("input")
-[print((o.name, o.type)) for o in sess.get_inputs()]
-print("output")
-[print((o.name, o.type)) for o in sess.get_outputs()]
-print(X[['bedrooms', 'bathrooms']][:1].to_numpy())
-print(X[['state', 'color']][:1].to_numpy(dtype=str))
-res = sess.run(None, {'numfeat': X[['bedrooms', 'bathrooms']][:1].to_numpy(dtype=np.float32), 'strfeat': X[['state', 'color']][:1].to_numpy(dtype=str)})
-print(res)
+# import onnxruntime as rt
+# sess = rt.InferenceSession("test/support/linear_regression/houses.onnx")
+# print("input")
+# [print((o.name, o.type)) for o in sess.get_inputs()]
+# print("output")
+# [print((o.name, o.type)) for o in sess.get_outputs()]
+# print(X[['bedrooms', 'bathrooms']][:1].to_numpy())
+# print(X[['state', 'color']][:1].to_numpy(dtype=str))
+# res = sess.run(None, {'numfeat': X[['bedrooms', 'bathrooms']][:1].to_numpy(dtype=np.float32), 'strfeat': X[['state', 'color']][:1].to_numpy(dtype=str)})
+# print(res)
